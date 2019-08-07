341b75a398ed719c01f597a7ede56cd27795cfae
Jonathan Pertile Morales <jonathan.morales@fortune.local>
2019-08-07 15:19:11 -0300

    Function to get dark o light color

--- dist/editable-note.js
+++ dist/editable-note.js
@@ -451,3 +451,32 @@ function setAttributes(el, attrs) {
 		el.setAttribute(key, attrs[key]);
 	}
 }
+function hex2(c) {
+	c = Math.round(c);
+	if (c < 0) c = 0;
+	if (c > 255) c = 255;
+
+	var s = c.toString(16);
+	if (s.length < 2) s = "0" + s;
+
+	return s;
+}
+function color(r, g, b) {
+	return "#" + hex2(r) + hex2(g) + hex2(b);
+}
+function shade(col, light) {
+	// TODO: Assert that col is good and that -1 < light < 1
+	var r = parseInt(col.substr(1, 2), 16);
+	var g = parseInt(col.substr(3, 2), 16);
+	var b = parseInt(col.substr(5, 2), 16);
+	if (light < 0) {
+		r = (1 + light) * r;
+		g = (1 + light) * g;
+		b = (1 + light) * b;
+	} else {
+		r = (1 - light) * r + light * 255;
+		g = (1 - light) * g + light * 255;
+		b = (1 - light) * b + light * 255;
+	}
+	return color(r, g, b);
+}
